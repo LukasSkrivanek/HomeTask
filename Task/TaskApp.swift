9d7af5d557d4f90fe01cc12072d17c1b2900349c
LukasSkrivanek <luky.skrivos@gmail.com>
2025-04-12 15:33:57 +0200

    Initial Commit

---
+++ Task/TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskApp.swift
+//  Task
+//
+//  Created by macbook on 12.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
